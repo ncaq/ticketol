f473082788360f34df60e80ef42e7aae0c5aa7ed
ncaq <ncaq@ncaq.net>
2015-12-03 16:46:17 +0900

    added: devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'top/show'
+
+  root to: "top#show"
+
   resources :concerts
   resources :events
   resources :contacts
   resources :reservations
   resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203065512) do
+ActiveRecord::Schema.define(version: 20151203074145) do
 
   create_table "concerts", force: :cascade do |t|
     t.text     "title"
-    t.integer  "artist_id"
+    t.text     "artist"
     t.binary   "image"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_index "concerts", ["artist_id"], name: "index_concerts_on_artist_id"
   add_index "concerts", ["user_id"], name: "index_concerts_on_user_id"
 
   create_table "contacts", force: :cascade do |t|
@@ -80,12 +79,21 @@ ActiveRecord::Schema.define(version: 20151203065512) do
   add_index "tickets", ["reservation_id"], name: "index_tickets_on_reservation_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
