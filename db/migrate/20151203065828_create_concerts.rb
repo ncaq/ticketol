ff921ed1dbd4cd09256aa47a4aa3bd841696a0be
ncaq <ncaq@ncaq.net>
2015-12-31 18:38:30 +0900

    added: concerts: user参照のnull禁止

--- db/migrate/20151203065828_create_concerts.rb
+++ db/migrate/20151203065828_create_concerts.rb
@@ -1,10 +1,10 @@
 class CreateConcerts < ActiveRecord::Migration
   def change
     create_table :concerts do |t|
-      t.text :title
-      t.text :artist
+      t.text :title, null: false
+      t.text :artist, null: false
       t.binary :image
-      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :user, index: true, foreign_key: true, null: false
 
       t.timestamps null: false
     end
