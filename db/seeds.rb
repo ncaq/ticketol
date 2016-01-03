961ed97ef7bd5d99c0c1b2cc0c47fe7a6c7c5979
ncaq <ncaq@ncaq.net>
2016-01-03 23:58:27 +0900

    added: seed: concertのseed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,13 @@ case Rails.env
 when "development"
   begin
     if ENV['ticketol_admin_password']
-      admin = User.create! do |u|
+      User.create! { |u|
         u.email = 'admin@example.com'
         u.name = 'admin'
         u.password = ENV['ticketol_admin_password']
         u.password_confirmation = u.password
         u.admin!
-      end
-      admin.save!
+      }
     else
       p 'May be you forgot'
       p 'export ticketol_admin_password=hogehoge'
@@ -27,27 +26,58 @@ when "development"
   end
 
   begin
-    buyer = User.create! do |u|
+    User.create! { |u|
       u.email = 'buyer@example.com'
       u.name = 'buyer'
       u.password = 'hogehoge'
       u.password_confirmation = u.password
       u.buyer!
-    end
-    buyer.save!
+    }
   rescue => e
     p e
   end
 
   begin
-    seller = User.create! do |u|
+    User.create! { |u|
       u.email = 'seller@example.com'
       u.name = 'seller'
       u.password = 'hogehoge'
       u.password_confirmation = u.password
       u.seller!
-    end
-    seller.save!
+    }
+  rescue => e
+    p e
+  end
+
+  begin
+    concert = Concert.new
+    concert.title = 'seed'
+    concert.artist = 'foo bar'
+    concert.user_id = User.where(name: 'seller').first.id
+    concert.save!
+
+    event = Event.new
+    event.place = 'hoge'
+    event.date       = DateTime.parse('2016-02-10')
+    event.sell_start = DateTime.parse('2016-02-01')
+    event.sell_end   = DateTime.parse('2016-02-09')
+    event.lottery = false
+    concert.events = [event]
+    event.save!
+
+    grade = Grade.new
+    grade.name = 's'
+    grade.price = '10000'
+    event.grades = [grade]
+    grade.save!
+
+    tickets = (100..150).map{ |i|
+      ticket = Ticket.new
+      ticket.seat = i.to_s
+      ticket
+    }
+    grade.tickets = tickets
+    tickets.each(&:save!)
   rescue => e
     p e
   end
