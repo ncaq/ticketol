2aa1a00458ce60c935e486ad36a4d78d6c095c68
ncaq <ncaq@ncaq.net>
2016-01-04 22:23:53 +0900

    changed: seeds: 例外キャッチやめる

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,86 +8,69 @@
 
 case Rails.env
 when "development"
-  begin
-    if ENV['ticketol_admin_password']
-      User.create! { |u|
-        u.email = 'admin@example.com'
-        u.name = 'admin'
-        u.password = ENV['ticketol_admin_password']
-        u.password_confirmation = u.password
-        u.admin!
-      }
-    else
-      p 'May be you forgot'
-      p 'export ticketol_admin_password=hogehoge'
-    end
-  rescue => e
-    p e
-  end
-
-  begin
+  if ENV['ticketol_admin_password']
     User.create! { |u|
-      u.email = 'buyer@example.com'
-      u.name = 'buyer'
-      u.password = 'hogehoge'
+      u.email = 'admin@example.com'
+      u.name = 'admin'
+      u.password = ENV['ticketol_admin_password']
       u.password_confirmation = u.password
-      u.buyer!
+      u.admin!
     }
-  rescue => e
-    p e
+  else
+    p 'May be you forgot'
+    p 'export ticketol_admin_password=hogehoge'
   end
 
-  begin
-    User.create! { |u|
-      u.email = 'seller@example.com'
-      u.name = 'seller'
-      u.password = 'hogehoge'
-      u.password_confirmation = u.password
-      u.seller!
+  User.create! { |u|
+    u.email = 'buyer@example.com'
+    u.name = 'buyer'
+    u.password = 'hogehoge'
+    u.password_confirmation = u.password
+    u.buyer!
+  }
+
+  User.create! { |u|
+    u.email = 'seller@example.com'
+    u.name = 'seller'
+    u.password = 'hogehoge'
+    u.password_confirmation = u.password
+    u.seller!
+  }
+
+  concert = Concert.new
+  concert.title = 'seed'
+  concert.artist = 'foo bar'
+  concert.user_id = User.where(name: 'seller').first.id
+  concert.save!
+
+  concert_image = ConcertImage.new
+  concert.concert_image = concert_image
+
+  event = Event.new
+  event.place = 'hoge'
+  event.date       = Time.zone.parse('2016-02-10')
+  event.sell_start = Time.zone.parse('2016-01-01')
+  event.sell_end   = Time.zone.parse('2016-02-09')
+  event.lottery = false
+  concert.events = [event]
+  event.save!
+
+  seat = 100
+  [['s', '10000'], ['a', '5000'], ['b', '2000']].each{ | (name, price) |
+    grade = Grade.new
+    grade.name = name
+    grade.price = price
+    event.grades << grade
+    grade.save!
+
+    tickets = (seat..seat + 50).map{ |i|
+      ticket = Ticket.new
+      ticket.seat = i.to_s
+      ticket
     }
-  rescue => e
-    p e
-  end
+    grade.tickets = tickets
+    tickets.each(&:save!)
 
-  begin
-    concert = Concert.new
-    concert.title = 'seed'
-    concert.artist = 'foo bar'
-    concert.user_id = User.where(name: 'seller').first.id
-    concert.save!
-
-    concert_image = ConcertImage.new
-    concert.concert_image = concert_image
-
-    event = Event.new
-    event.place = 'hoge'
-    event.date       = Time.zone.parse('2016-02-10')
-    event.sell_start = Time.zone.parse('2016-01-01')
-    event.sell_end   = Time.zone.parse('2016-02-09')
-    event.lottery = false
-    concert.events = [event]
-    event.save!
-
-    seat = 100
-    [['s', '10000'], ['a', '5000'], ['b', '2000']].each{ | (name, price) |
-      grade = Grade.new
-      grade.name = name
-      grade.price = price
-      event.grades << grade
-      grade.save!
-
-      tickets = (seat..seat + 50).map{ |i|
-        ticket = Ticket.new
-        ticket.seat = i.to_s
-        ticket
-      }
-      grade.tickets = tickets
-      tickets.each(&:save!)
-
-      seat += 50
-    }
-
-  rescue => e
-    p e
-  end
+    seat += 50
+  }
 end
