56d543a3fb72944e84b587ea0d41e7ae149d902e
ncaq <ncaq@ncaq.net>
2016-01-04 15:18:54 +0900

    added: concert: 購入リンク追加

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,14 +4,45 @@ class ReservationsController < ApplicationController
   # GET /reservations/1
   # GET /reservations/1.json
   def show
-    if current_user && (current_user.admin? || current_user.id == @reservation.user_id)
-      allow
-    end
+    has?
+  end
+
+  def new
+    write?
+  end
+
+  def create
+    write?
+  end
+
+  def destroy
+    has?
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_reservation
-      @reservation = Reservation.find(params[:id])
+  # Use callbacks to share common setup or constraints between actions.
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def has?
+    if current_user && (current_user.admin? || current_user.id == @reservation.user_id)
+      allow
+    else
+      deny
     end
+  end
+
+  def write?
+    if current_user
+      if current_user.buyer?
+        allow
+      else
+        deny
+      end
+    else
+      redirect_to new_user_session_url
+      allow
+    end
+  end
 end
