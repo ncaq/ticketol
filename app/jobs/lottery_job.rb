742d7dd733f1e855e48c30758334b6137ff51d3f
ncaq <ncaq@ncaq.net>
2016-01-20 21:34:59 +0900

    updated: lottery_job

--- app/jobs/lottery_job.rb
+++ app/jobs/lottery_job.rb
@@ -3,22 +3,21 @@ class LotteryJob < ActiveJob::Base
   queue_as :default
 
   def perform((event))
-    grades = event.grades
-    grades.each { |grade|
-      while true
-        open_tickets = grade.tickets.where(reservation_id: nil)
-        pendings = grade.lottery_pendings
-        if open_tickets.empty? || pendings.empty?
-          break
+    ActiveRecord::Base.transaction {
+      event.grades.each { |grade|
+        inventory = grade.tickets.inventory
+        pendings = grade.reservations.lottery_pending
+        until inventory.empty? || pendings.empty?
+          elected = pendings.sample
+          if elected.volume <= inventory.length
+            elected.buy(inventory.take(elected.volume).map(&:id))
+          else
+            elected.lose!
+          end
+          inventory = grade.tickets.inventory
+          pendings = grade.reservations.lottery_pending
         end
-        elected_reservation = pendings.sample.reservation
-        elected_lottery_pendings = elected_reservation.lottery_pendings # 連番
-        if elected_lottery_pendings.length <= open_tickets.length
-          elected_reservation.buy(open_tickets.take(10).map(&:id))
-        elsif open_tickets.length < pendings.group(:reservation_id).count.values.min
-          break
-        end
-      end
+      }
     }
   end
 end
