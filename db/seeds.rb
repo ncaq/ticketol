980ca167461705e0c3dc92610cfc0ad1e72cfafd
ncaq <ncaq@ncaq.net>
2016-01-21 12:43:43 +0900

    changed: seeds: inc sell_end random range

--- db/seeds.rb
+++ db/seeds.rb
@@ -64,9 +64,9 @@ when 'development'
     rand(1..5).times {
       event = Event.create!{ |e|
         e.place      = random_word()
-        e.date       = rand(60*3..60*60*24*5).second.from_now
+        e.date       = rand(60*10..60*60*24*5).second.from_now
         e.sell_start = rand(0..60*3).second.from_now
-        e.sell_end   = rand(0..60*3).second.from_now
+        e.sell_end   = rand(0..60*10).second.from_now
         e.lottery    = rand(0..1)
         e.concert    = concert
       }
