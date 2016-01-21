5cc994502e33e201238d964e25a783ae7d9a1e74
ncaq <ncaq@ncaq.net>
2016-01-21 16:48:07 +0900

    fixed: ticket order from id

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,5 +2,5 @@ class Ticket < ActiveRecord::Base
   belongs_to :grade
   belongs_to :reservation
 
-  scope :inventory, -> { where(reservation_id: nil) }
+  scope :inventory, -> { where(reservation_id: nil).order(:id) }
 end
