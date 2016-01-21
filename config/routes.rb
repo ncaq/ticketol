b4fbcadc6a553072444b30294b56df1e1f2f5b88
ncaq <ncaq@ncaq.net>
2016-01-21 12:07:44 +0900

    deleted: devise account cancel

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   root to: "top#show"
 
-  devise_for :users, controllers: {
-               registrations: 'users/registrations'
-             }
+  devise_for :users, skip: :registrations
+  devise_scope(:user) {
+    resource :registration,
+             only: [:new, :create, :edit, :update],
+             path: 'users',
+             path_names: { new: 'sign_up' },
+             controller: 'users/registrations',
+             as: :user_registration do
+      get :cancel
+    end
+  }
   resources :users,        :only => [:index, :show, :edit, :update]
 
   resources :concerts,     :only => [:index, :show, :new, :create, :edit, :update] { || member { || get 'image' } }
